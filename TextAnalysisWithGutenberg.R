08565d8378987b5c36dd9d1fc7fc3e54434843ae
Anna Grigoryan <36204094+thegrigorian@users.noreply.github.com>
2019-04-30 13:04:08 +0200

    Add files via upload

---
+++ TextAnalysisWithGutenberg.R
@@ -0,0 +1,310 @@
+
+library(gutenbergr)
+library(tidytext)
+library(dplyr)
+library(tidyverse)
+library(stringr)
+library(tm)
+library(topicmodels)
+library(ggplot2)
+
+gutenberg_metadata
+##################################################################
+#################  CHARLES DICKENS ###############################
+gutenberg_works(author == "Dickens, Charles")
+
+## selecting the first 5 books written by Charles Dickens
+books <- gutenberg_download(c(46, 98, 564, 580, 588), meta_fields = "title")
+books
+
+
+# divide into documents, each representing one chapter
+by_chapter <- books %>%
+  group_by(title) %>%
+  mutate(chapter = cumsum(str_detect(text, regex("^chapter ", ignore_case = TRUE)))) %>%
+  ungroup() %>%
+  filter(chapter > 0) %>%
+  unite(document, title, chapter)
+
+
+# split into words
+by_chapter_word <- by_chapter %>%
+  unnest_tokens(word, text)
+
+# find document-word counts
+word_counts <- by_chapter_word %>%
+  anti_join(stop_words) %>%
+  count(document, word, sort = TRUE) %>%
+  ungroup()
+
+word_counts
+
+book_words <- books %>%
+  unnest_tokens(word, text) %>%
+  count(title, word, sort = TRUE)
+
+total_words <- book_words %>% 
+  group_by(title) %>% 
+  summarize(total = sum(n))
+
+book_words <- left_join(book_words, total_words)
+
+book_words
+
+
+ggplot(book_words, aes(n/total, fill = title)) +
+  geom_histogram(show.legend = FALSE) +
+  xlim(NA, 0.0009) +
+  facet_wrap(~title, ncol = 2, scales = "free_y")
+
+freq_by_rank <- book_words %>% 
+  group_by(title) %>% 
+  mutate(rank = row_number(), 
+         `term frequency` = n/total)
+
+freq_by_rank
+
+freq_by_rank %>% 
+  ggplot(aes(rank, `term frequency`, color = title)) + 
+  geom_line(size = 1.1, alpha = 0.8, show.legend = FALSE) + 
+  scale_x_log10() +
+  scale_y_log10()
+
+
+
+rank_subset <- freq_by_rank %>% 
+  filter(rank < 500,
+         rank > 10)
+
+lm(log10(`term frequency`) ~ log10(rank), data = rank_subset)
+
+freq_by_rank %>% 
+  ggplot(aes(rank, `term frequency`, color = title)) + 
+  geom_abline(intercept = -0.62, slope = -1.1, color = "gray50", linetype = 2) +
+  geom_line(size = 1.1, alpha = 0.8, show.legend = FALSE) + 
+  scale_x_log10() +
+  scale_y_log10()
+
+chapters_dtm <- word_counts %>%
+  cast_dtm(document, word, n)
+
+book_words <- book_words %>%
+  bind_tf_idf(word, title, n)
+book_words
+
+
+book_words %>%
+  select(-total) %>%
+  arrange(desc(tf_idf))
+
+book_words %>%
+  arrange(desc(tf_idf)) %>%
+  mutate(word = factor(word, levels = rev(unique(word)))) %>% 
+  group_by(title) %>% 
+  top_n(15) %>% 
+  ungroup() %>%
+  ggplot(aes(word, tf_idf, fill = title)) +
+  geom_col(show.legend = FALSE) +
+  labs(x = NULL, y = "tf-idf") +
+  facet_wrap(~title, ncol = 2, scales = "free") +
+  coord_flip()
+  
+##################################################################
+#################  ARTHUR CONAN DOYLE ############################
+
+gutenberg_works(author == "Doyle, Arthur Conan")
+books <- gutenberg_download(c(108, 126, 139, 244, 290), meta_fields = "title")
+books
+
+
+
+# divide into documents, each representing one chapter
+by_chapter <- books %>%
+  group_by(title) %>%
+  mutate(chapter = cumsum(str_detect(text, regex("^chapter ", ignore_case = TRUE)))) %>%
+  ungroup() %>%
+  filter(chapter > 0) %>%
+  unite(document, title, chapter)
+
+# split into words
+by_chapter_word <- by_chapter %>%
+  unnest_tokens(word, text)
+
+# find document-word counts
+word_counts <- by_chapter_word %>%
+  anti_join(stop_words) %>%
+  count(document, word, sort = TRUE) %>%
+  ungroup()
+
+word_counts
+
+book_words <- books %>%
+  unnest_tokens(word, text) %>%
+  count(title, word, sort = TRUE)
+
+total_words <- book_words %>% 
+  group_by(title) %>% 
+  summarize(total = sum(n))
+
+book_words <- left_join(book_words, total_words)
+
+book_words
+
+
+ggplot(book_words, aes(n/total, fill = title)) +
+  geom_histogram(show.legend = FALSE) +
+  xlim(NA, 0.0009) +
+  facet_wrap(~title, ncol = 2, scales = "free_y")
+
+freq_by_rank <- book_words %>% 
+  group_by(title) %>% 
+  mutate(rank = row_number(), 
+         `term frequency` = n/total)
+
+freq_by_rank
+
+freq_by_rank %>% 
+  ggplot(aes(rank, `term frequency`, color = title)) + 
+  geom_line(size = 1.1, alpha = 0.8, show.legend = FALSE) + 
+  scale_x_log10() +
+  scale_y_log10()
+
+
+
+rank_subset <- freq_by_rank %>% 
+  filter(rank < 500,
+         rank > 10)
+
+lm(log10(`term frequency`) ~ log10(rank), data = rank_subset)
+
+freq_by_rank %>% 
+  ggplot(aes(rank, `term frequency`, color = title)) + 
+  geom_abline(intercept = -0.62, slope = -1.1, color = "gray50", linetype = 2) +
+  geom_line(size = 1.1, alpha = 0.8, show.legend = FALSE) + 
+  scale_x_log10() +
+  scale_y_log10()
+
+chapters_dtm <- word_counts %>%
+  cast_dtm(document, word, n)
+
+book_words <- book_words %>%
+  bind_tf_idf(word, title, n)
+book_words
+
+
+book_words %>%
+  select(-total) %>%
+  arrange(desc(tf_idf))
+
+book_words %>%
+  arrange(desc(tf_idf)) %>%
+  mutate(word = factor(word, levels = rev(unique(word)))) %>% 
+  group_by(title) %>% 
+  top_n(15) %>% 
+  ungroup() %>%
+  ggplot(aes(word, tf_idf, fill = title)) +
+  geom_col(show.legend = FALSE) +
+  labs(x = NULL, y = "tf-idf") +
+  facet_wrap(~title, ncol = 2, scales = "free") +
+  coord_flip()
+
+
+
+##################################################################
+#################  OSCAR WILDE     ###############################
+gutenberg_works(author == "Wilde, Oscar")
+
+books <- gutenberg_download(c(174, 301, 773, 774, 790), meta_fields = "title")
+books
+
+
+# divide into documents, each representing one chapter
+by_chapter <- books %>%
+  group_by(title) %>%
+  mutate(chapter = cumsum(str_detect(text, regex("^chapter ", ignore_case = TRUE)))) %>%
+  ungroup() %>%
+  filter(chapter > 0) %>%
+  unite(document, title, chapter)
+
+# split into words
+by_chapter_word <- by_chapter %>%
+  unnest_tokens(word, text)
+
+# find document-word counts
+word_counts <- by_chapter_word %>%
+  anti_join(stop_words) %>%
+  count(document, word, sort = TRUE) %>%
+  ungroup()
+
+word_counts
+
+book_words <- books %>%
+  unnest_tokens(word, text) %>%
+  count(title, word, sort = TRUE)
+
+total_words <- book_words %>% 
+  group_by(title) %>% 
+  summarize(total = sum(n))
+
+book_words <- left_join(book_words, total_words)
+
+book_words
+
+
+ggplot(book_words, aes(n/total, fill = title)) +
+  geom_histogram(show.legend = FALSE) +
+  xlim(NA, 0.0009) +
+  facet_wrap(~title, ncol = 2, scales = "free_y")
+
+freq_by_rank <- book_words %>% 
+  group_by(title) %>% 
+  mutate(rank = row_number(), 
+         `term frequency` = n/total)
+
+freq_by_rank
+
+freq_by_rank %>% 
+  ggplot(aes(rank, `term frequency`, color = title)) + 
+  geom_line(size = 1.1, alpha = 0.8, show.legend = FALSE) + 
+  scale_x_log10() +
+  scale_y_log10()
+
+
+
+rank_subset <- freq_by_rank %>% 
+  filter(rank < 500,
+         rank > 10)
+
+lm(log10(`term frequency`) ~ log10(rank), data = rank_subset)
+
+freq_by_rank %>% 
+  ggplot(aes(rank, `term frequency`, color = title)) + 
+  geom_abline(intercept = -0.62, slope = -1.1, color = "gray50", linetype = 2) +
+  geom_line(size = 1.1, alpha = 0.8, show.legend = FALSE) + 
+  scale_x_log10() +
+  scale_y_log10()
+
+chapters_dtm <- word_counts %>%
+  cast_dtm(document, word, n)
+
+book_words <- book_words %>%
+  bind_tf_idf(word, title, n)
+book_words
+
+
+book_words %>%
+  select(-total) %>%
+  arrange(desc(tf_idf))
+
+book_words %>%
+  arrange(desc(tf_idf)) %>%
+  mutate(word = factor(word, levels = rev(unique(word)))) %>% 
+  group_by(title) %>% 
+  top_n(15) %>% 
+  ungroup() %>%
+  ggplot(aes(word, tf_idf, fill = title)) +
+  geom_col(show.legend = FALSE) +
+  labs(x = NULL, y = "tf-idf") +
+  facet_wrap(~title, ncol = 2, scales = "free") +
+  coord_flip()
+
